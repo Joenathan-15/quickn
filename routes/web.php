a851e5944413cb1e6cb469ed8709983a04f2461f
Joenathan <hazendas10@gmail.com>
2024-10-08 09:32:01 +0700

    feat:add Sidebar ui (no functionality)

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
+use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/',Welcome::class);
 Route::get('/login',Login::class);
 Route::get('/register',Register::class);
